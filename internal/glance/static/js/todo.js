0fa705575bba69ab61f43a3beb04ac0471f48fa1
rakkateichou <rakkateichou@gmail.com>
2026-04-25 23:46:59 +0800

    debug: add websocket logging

--- internal/glance/static/js/todo.js
+++ internal/glance/static/js/todo.js
@@ -13,12 +13,18 @@ function initSync() {
     function connect() {
         const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
         const url = `${protocol}//${window.location.host}${pageData.baseURL}/api/sync`;
+        console.log(`[Todo Sync] Attempting to connect to ${url}`);
+        
         socket = new WebSocket(url);
 
-        socket.onopen = () => clearTimeout(reconnectTimer);
+        socket.onopen = () => {
+            console.log(`[Todo Sync] Connected to ${url}`);
+            clearTimeout(reconnectTimer);
+        };
 
         socket.onmessage = (event) => {
             const data = JSON.parse(event.data);
+            console.log(`[Todo Sync] Received update for ${data.key}`);
             
             // Only update if the incoming data is actually new
             if (localStorage.getItem(data.key) !== data.value) {
@@ -31,7 +37,12 @@ function initSync() {
             }
         };
 
+        socket.onerror = (error) => {
+            console.error(`[Todo Sync] WebSocket Error:`, error);
+        };
+
         socket.onclose = () => {
+            console.log(`[Todo Sync] Connection closed. Retrying in 3s...`);
             reconnectTimer = setTimeout(connect, 3000);
         };
     }
